880ab1b32665cd78f0edf9b3cabb08e61df98599
Reuben <reuben@koppenheffer.me>
2019-07-01 19:26:22 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace adventure_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adventure Game!");
+            Console.ReadKey();
+        }
+    }
+}
