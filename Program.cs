bee6ae74b0dfa1b5adb0adae2ff288001dfbf0d5
Reuben <reuben@koppenheffer.me>
2019-07-02 22:05:34 -0500

    Add new title and welcome message

--- Program.cs
+++ Program.cs
@@ -6,8 +6,11 @@ namespace adventure_game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Adventure Game!");
-            Console.ReadKey();
+            Console.WriteLine("Let's Go Adventuring!");
+            Console.Read();
+
+            Console.WriteLine("Welcome to this gnarly adventure game. Which direction will you want to go?");
+            Console.Read();
         }
     }
 }
