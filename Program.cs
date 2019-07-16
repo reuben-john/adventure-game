9651b1318e7d699ce5e7089145370e1549d05f40
Reuben <reuben@koppenheffer.me>
2019-07-15 22:35:56 -0500

    Add prompt for name

--- Program.cs
+++ Program.cs
@@ -6,11 +6,15 @@ namespace adventure_game
     {
         static void Main(string[] args)
         {
+
+            string characterName;
             Console.WriteLine("Let's Go Adventuring!");
             Console.Read();
 
             Console.WriteLine("Welcome to this gnarly adventure game. Which direction will you want to go?");
-            Console.Read();
+            Console.Write("What name would you like to us? ");
+            characterName = Console.ReadLine();
+            Console.WriteLine($"Welcome {characterName}, please enjoy the game!");
         }
     }
 }
