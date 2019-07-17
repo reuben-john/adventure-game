624f10ec2053d523f95f438f4e4b8f1b45f386a1
Reuben <reuben@koppenheffer.me>
2019-07-17 06:13:17 -0500

    Finish test game

--- Program.cs
+++ Program.cs
@@ -7,14 +7,15 @@ namespace adventure_game
     {
 
         static int Scenarios = 3;
+        static string CharacterName;
 
         static string[] PartOne = {
             "At the front of the imposing building you see a weathered old man with a cart.\nAs you near, you see the cart is filled with what looks like mostly junk and \nonly a few useful items. All you have on you is piece of a chalk.\nYou offer it to him, and he says he'll trade a flashlight or an umbrella for it.\nTo choose type either A for the flashlight, or B for the umbrella.",
             "The power in the building goes out - luckily you have a flashlight! \nYou move the light around and a large animal is frightened by the \nsudden brightness and takes off. As you move the light again, \nsomething glitters. You reach down and pick up a coin!",
             "The power in the building goes out! As you move down the hallway \nyou hear what sounds like a large animal nearby. You move the \numbrella in a widening arc in front of you to scare it, \nand the animal skitters off.",
             "The lights return and you move into a room at the end of the hall. \nThere is a vending machine.",
-            "Luckily " + characterName + " you have that coin you found and you buy yourself a snack.",
-            "Too bad you don't have a coin on you," + characterName + ",\nor you would have been able to get a snack.",
+            "Luckily " + CharacterName + " you have that coin you found and you buy yourself a snack.",
+            "Too bad you don't have a coin on you," + CharacterName + ",\nor you would have been able to get a snack.",
             "You begin to climb the stairs to the next floor."
         };
         static string[] PartTwo = {
@@ -35,14 +36,172 @@ namespace adventure_game
             "... part 3 again - what happens if B is chosen...",
             "You begin to climb the stairs to the next floor...."
         };
-        static string characterName = "John Doe";
 
         public static void StartGame()
         {
-            // Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Clear();
-            string title = @"
+            GameTitle();
+
+            //introduction text
+            Console.WriteLine("You are about to enter the headquarters of your arch nemesis.");
+
+            NameCharacter();
+            Choice();
+            EndGame();
+        }
+
+        public static void EndGame()
+        {
+
+            //end of game text
+            Console.WriteLine("End of story text here.....");
+            Console.WriteLine("Congratulations " + CharacterName + "!");
+
+
+        }
+        static void Choice()
+        {
+            for (int section = 1; section <= Scenarios; section++)
+            {
+                string input = "";
+
+                switch (section)
+                {
+                    case 1:
+                        //Part One
+
+                        //same pattern for each of the sections. 1) print the first part of the section
+                        Console.WriteLine(PartOne[0]);
+
+                        //2)read in player's choice (a or b)
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        Console.Write("Enter your choice: ");
+                        input = Console.ReadLine();
+                        input = input.ToLower();
+                        Console.ResetColor();
+
+                        //3) if a print the next part of the array, otherwise skip next and print 3rd
+                        if (input == "a")
+                        {
+                            Console.WriteLine(PartOne[1]);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(PartOne[2]);
+                        }
+
+                        //4) print next part of the section
+                        Console.WriteLine(PartOne[3]);
+
+                        //5) again if a print next, otherwise skip ahead
+                        if (input == "a")
+                        {
+                            Console.WriteLine(PartOne[4]);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(PartOne[5]);
+                        }
+
+                        //6) print last piece of the section
+                        Console.WriteLine(PartOne[6]);
+
+
+
+                        break;
+
+                    case 2:
+                        //Part Two
+
+                        Console.WriteLine(PartTwo[0]);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Enter your choice: ");
+                        input = Console.ReadLine();
+                        input = input.ToLower();
+                        Console.ResetColor();
+                        if (input == "a")
+                        {
+                            Console.WriteLine(PartTwo[1]);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(PartTwo[2]);
+                        }
+                        Console.WriteLine(PartTwo[3]);
+                        if (input == "a")
+                        {
+                            Console.WriteLine(PartTwo[4]);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(PartTwo[5]);
+                        }
+                        Console.WriteLine(PartTwo[6]);
+
+                        break;
+
+                    case 3:
+                        //Part Three
+                        Console.WriteLine(PartThree[0]);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Enter your choice: ");
+                        input = Console.ReadLine();
+                        input = input.ToLower();
+                        Console.ResetColor();
+                        if (input == "a")
+                        {
+                            Console.WriteLine(PartThree[1]);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(PartThree[2]);
+                        }
+                        Console.WriteLine(PartThree[3]);
+                        if (input == "a")
+                        {
+                            Console.WriteLine(PartThree[4]);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(PartThree[5]);
+                        }
+                        Console.WriteLine(PartThree[6]);
+                        break;
+
+                    default:
+                        //default if section number does not match one of the above
+                        break;
+                }
+
+                //let player advance when ready, then clear the screen
+                Console.WriteLine("Press enter to continue...");
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+        }
+        static void NameCharacter()
+        {
+            Console.WriteLine("You need a code name for this mission. What will it be?");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Enter your code name: ");
+
+            CharacterName = Console.ReadLine();
+
+            Console.ResetColor();
+            Console.WriteLine("Your code name is confirmed to be " + CharacterName + ". Good luck!\n\n");
+
+        }
+
+        static void GameTitle()
+        {
+            string Title = @"
 
              __    ____  ____  _ ____     ___   __              
             (  )  (  __)(_  _)(// ___)   / __) /  \             
@@ -57,52 +216,30 @@ namespace adventure_game
 
 
 ";
-            Console.Write(title);
+            Console.Title = Title;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            //game title
+            Console.WriteLine(Title);
+            //game slogan/subtitle
+            Console.WriteLine("An Awesome Adventure Game");
             Console.ResetColor();
-            Console.WriteLine("Let's Go Adventuring!");
-            Console.WriteLine("Welcome to this gnarly adventure game. Which direction will you want to go?");
-            NameCharacter();
-            Choice();
-
-        }
-        public static void NameCharacter()
-        {
-            Console.Write("What name would you like to us? ");
-            characterName = Console.ReadLine();
-            Console.WriteLine($"Welcome {characterName}, please enjoy the game!");
-
-        }
-        static void Choice()
-        {
-            string input = "";
-            Console.WriteLine($"Make a choice {characterName}; A or B?");
-            input = Console.ReadLine();
-            input = input.ToUpper();
-            if (input == "A")
-            {
-                Console.WriteLine("You've chosen path A!");
-            }
-            else
-            {
-                Console.WriteLine("You've chosen path B!");
-            }
-        }
-        public static void EndGame()
-        {
-            //end of game text
-            Console.WriteLine("End of story text here.....");
-
-            Console.WriteLine("Press enter to exit.");
+            Console.WriteLine("Press enter to start");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
+    class Item
+    {
 
-    class Item { }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-
             Game.StartGame();
+            Console.Read();
+
         }
     }
+
 }
